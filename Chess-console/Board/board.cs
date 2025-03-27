ae8be7b1daa45bd71e9ab91b4c8ac501b901c806
Guilherme Soares <guicesarsosil@gmail.com>
2025-03-27 11:13:05 -0300

    Starting Method that shows board on the sreen

--- Chess-console/Board/board.cs
+++ Chess-console/Board/board.cs
@@ -6,12 +6,18 @@ namespace board
         public int lines { get; set; }
         public int columns { get; set; }
 
-        private Parts[,] parts;
+        private Parts[,] parts; // precaution
         public Board(int lines, int columns)
         {
             this.lines = lines;
             this.columns = columns;
             parts = new Parts[lines, columns];
+        }
+
+        public Parts part(int lines, int columns)
+        {
+            return parts[lines, columns];
+            // method to access the private matrix
         }
     }
 }
---
+++ Chess-console/Screen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using board;
+
+namespace chess_console
+{
+    class Screen
+    {
+        public static void printBoard(Board board)
+        {
+            for (int i = 0; i < board.lines; i++)
+            {
+                for (int j = 0; j < board.columns; j++)
+                {
+                    if (board.part(i, j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write(board.part(i, j) + " ");
+
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+// board.part is precisely the method created so that we can access the private matrix.
