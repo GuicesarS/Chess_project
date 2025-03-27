197192139cb13b4d7b1e654391b0127f50636096
Guilherme Soares <guicesarsosil@gmail.com>
2025-03-27 10:37:19 -0300

    Board Implementation

---
+++ Chess-console/Board/Parts.cs
@@ -0,0 +1,20 @@
+﻿
+namespace board
+{
+    class Parts
+    {
+        public Position position { get; set; }
+        public Color color { get; protected set; }
+
+        public int amountofmoviment { get; protected set; }
+        public Board board{ get; protected set; }
+
+        public Parts(Position position, Board board, Color color)
+        {
+            this.position = position;
+            this.board = board;
+            this.color = color;
+            this.amountofmoviment = 0;
+        }
+    }
+}
---
+++ Chess-console/Board/board.cs
@@ -0,0 +1,17 @@
+﻿
+namespace board
+{
+    class Board
+    {
+        public int lines { get; set; }
+        public int columns { get; set; }
+
+        private Parts[,] parts;
+        public Board(int lines, int columns)
+        {
+            this.lines = lines;
+            this.columns = columns;
+            parts = new Parts[lines, columns];
+        }
+    }
+}
