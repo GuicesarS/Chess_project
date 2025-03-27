f9ff5471f7e29b977cd0ca797c676f683f614e67
Guilherme Soares <guicesarsosil@gmail.com>
2025-03-27 10:16:49 -0300

    Created Position Class

---
+++ Chess-console/Board/Position.cs
@@ -0,0 +1,21 @@
+﻿
+namespace board
+{
+    class Position
+    {
+        public int lines { get; set; }
+        public int columns { get; set; }
+
+        public Position (int lines, int columns)
+        {
+            this.lines = lines;
+            this.columns = columns;
+        }
+
+        public override string ToString()
+        {
+            return $"{lines} , {columns}";
+         }
+
+    }
+}
--- Chess-console/Program.cs
+++ Chess-console/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using board;
+
+namespace chess_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position p = new Position(3, 4);
+            Console.WriteLine($"Position : {p}");
+
+            Console.ReadLine();
+        }
+    }
+}
