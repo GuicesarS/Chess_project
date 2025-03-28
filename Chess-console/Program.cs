ca83e68427ba63f3a94659d8da6eceba92daffbd
Guilherme Soares <guicesarsosil@gmail.com>
2025-03-27 22:39:24 -0300

    Improved the implementation of the  method and changed the background to differentiate black pieces

--- Chess-console/Program.cs
+++ Chess-console/Program.cs
@@ -8,11 +8,23 @@ namespace chess_console
     {
         static void Main(string[] args)
         {
-            PositionChess pos = new PositionChess('c', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.convertToPosition());
+            try
+            {
+                Board board = new Board(8, 8);
 
-            Console.ReadLine();
+                board.putPiece(new Tower (board,Color.Black), new Position(0, 0));
+                board.putPiece(new Tower(board, Color.Black), new Position(3, 0));
+                board.putPiece(new King(board, Color.Black), new Position(1, 5));
+                board.putPiece(new King(board, Color.White), new Position(1, 3));
+                board.putPiece(new Tower(board, Color.White), new Position(2, 5));
+                Screen.printBoard(board);
+
+                Console.ReadLine();
+            }
+            catch(BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            }
            
         }
     }
